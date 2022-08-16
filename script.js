0da31a45100ef17972d369dc0472bcd0603235c3
qodirolimov <qodirolimov88@gmail.com>
2022-08-16 08:48:39 +0500

    next

---
+++ script.js
@@ -0,0 +1,61 @@
+"use strict"
+
+
+  // 2-dars for()
+         //  4-masala
+ 
+// for (let i =1; i<=10; i++){
+//    console.log(i*1+"kg "
+//    +i/1*10000+ "so'm");
+// }
+
+
+            //  5-masala
+// for (let i =1; i<=10; i++){
+//    console.log(i/10+"kg"
+//    +i/1*10000 + "so'm");
+// }
+
+
+//                  6-masala
+// for (let i =5; i<=10; i++){
+//    console.log(i/5+"kg "
+//    +i/5*10000 + "so'm");
+// }
+
+
+
+                  //  11-masala
+// for (let i =1; i<=21; i++){
+//    console.log(i++);
+// }
+
+
+
+//                    12-masala
+// for (let i =2; i<=20; i++){
+//    console.log(i++);
+// }
+
+
+//                13-masala
+// for (let i=1; i<=40; i++){
+//    console.log(i*50)   
+// }                 
+
+
+                  //  15-masala
+// for (let i =1; i<=20; i++){
+//    console.log(-i+20);
+// }
+
+
+                  // 16-masala
+// for (let i=0; i<=10; i++){
+//    console.log(i*5)   
+// }                 
+
+
+
+
+
